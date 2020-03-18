849922a89e6137f98b5d22a7ca4e7cb975d5825e
zainlateef <zlateefwps@gmail.com>
2020-03-18 07:46:33 -0400

    first iteration for mac works

--- NatureIsLitBackgrounds.py
+++ NatureIsLitBackgrounds.py
@@ -1,5 +1,5 @@
 import subprocess
-import wget
+import requests
 import os
 
 APPLESCRIPT = """/usr/bin/osascript<<END
@@ -15,6 +15,7 @@ currentBackgroundImageName = 'currentBackground'
 def set_desktop_background(url):
     filepath = download_file_from_url(url)
     subprocess.Popen(APPLESCRIPT % filepath, shell=True)
+    subprocess.check_call("killall Dock", shell=True)
 
 
 def download_file_from_url(url):
@@ -22,9 +23,12 @@ def download_file_from_url(url):
     filepath = cwd + '/' + currentBackgroundImageName + extension
     if os.path.exists(filepath):
         os.remove(filepath)
-    wget.download(url, filepath)
+    response = requests.get(url, timeout=5)
+    if response.ok:
+        with open(filepath, 'wb') as f:
+            f .write(response.content)
     return filepath
 
 
 if __name__ == "__main__":
-    set_desktop_background("https://via.placeholder.com/150.png")
+    set_desktop_background("https://via.placeholder.com/200.png")
