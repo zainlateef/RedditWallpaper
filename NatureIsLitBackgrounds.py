c2223d5c9e462a9030abf6cbae876225421aad06
zainlateef <zlateefwps@gmail.com>
2020-03-18 16:00:00 -0400

    file module finalized. should separate into a new file

--- NatureIsLitBackgrounds.py
+++ NatureIsLitBackgrounds.py
@@ -1,6 +1,8 @@
 import subprocess
 import requests
 import os
+import ctypes
+from platform import system
 
 APPLESCRIPT = """/usr/bin/osascript<<END
 tell application "Finder"
@@ -8,26 +10,41 @@ set desktop picture to POSIX file "%s"
 end tell
 END"""
 
+current_os = system()
 cwd = os.getcwd()
 currentBackgroundImageName = 'currentBackground'
 
 
 def set_desktop_background(url):
     filepath = download_file_from_url(url)
+    if os.name == 'posix':
+        changeMacBackground(filepath)
+    elif os.name == 'nt':
+        changeWindowsBackground(filepath)
+    else:
+        sys.exit('Your os is not supported')
+
+
+def changeMacBackground(filepath):
     subprocess.Popen(APPLESCRIPT % filepath, shell=True)
     subprocess.check_call("killall Dock", shell=True)
 
+def changeWindowsBackground(filepath):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath , 0)
 
 def download_file_from_url(url):
     extension = url[url.rfind('.'):]
     filepath = cwd + '/' + currentBackgroundImageName + extension
-    if os.path.exists(filepath):
-        os.remove(filepath)
+    deleteFileIfExists(filepath)
     response = requests.get(url, timeout=5)
     if response.ok:
         with open(filepath, 'wb') as f:
             f .write(response.content)
     return filepath
+
+def deleteFileIfExists(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
 
 
 if __name__ == "__main__":
