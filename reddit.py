8fdd7f78c98a5239f3475ed1bef16853bcd6e4ac
zainlateef <zlateefwps@gmail.com>
2020-03-23 14:45:34 -0400

    formatted

--- reddit.py
+++ reddit.py
@@ -1,8 +1,18 @@
 import praw
+
+def isImageUrl(url):
+    return url.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff'))
+
 r = praw.Reddit(
     client_id='dl7NsHAAfRgzOA',
     client_secret='XyxGqNFc3Lt-fDF-MkP93ECa9IQ',
     user_agent='nature_is_lit_wallpaper_app')
 
-for submission in r.subreddit('natureisfuckinglit').hot(limit=30):
-    print(submission.title)
+
+
+def getRedditUrls():
+    urls = []
+    for sub in r.subreddit('natureisfuckinglit').hot(limit=30):
+        if(getattr(sub,"over_18",True)==False and getattr(sub,"post_hint","")=='image'and isImageUrl(getattr(sub,"url",""))):
+            urls.append(sub.url)
+# post_hint: image
